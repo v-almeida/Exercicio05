9367b829f1a0fd6473ef3610b144fac6d1b42e65
Vítor <almeida.vitor20000@gmail.com>
2024-05-20 20:10:30 -0300

    try and catch

--- ExemploEx/src/App.java
+++ ExemploEx/src/App.java
@@ -3,16 +3,25 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws FileNotFoundException {
-        // LER O ARQUIVO DE TEXTO
-        File arquivoLeitura = new File(
-                "C:\\Users\\autologon\\Documents\\vitor-java\\lista05\\Exercicio05\\ExemploEx\\src\\arquivo.txt");
-        Scanner scanner = new Scanner(arquivoLeitura);
-        // Enquanto estiver linha no arquivo
-        while (scanner.hasNextLine()) {
-            String linha = scanner.nextLine();
-            System.out.println(linha);
+    public static void main(String[] args) { // throws FileNotFoundException {
+
+        try {
+            // LER O ARQUIVO DE TEXTO
+            File arquivoLeitura = new File(
+                    "C:\\Users\\autologon\\Documents\\vitor-java\\lista05\\Exercicio05\\ExemploEx\\src\\arquivo.txt");
+            Scanner scanner = new Scanner(arquivoLeitura);
+            // Enquanto estiver linha no arquivo
+            while (scanner.hasNextLine()) {
+                String linha = scanner.nextLine();
+                System.out.println(linha);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado!" + e.getMessage());
+
         }
+
+        // PROCESSAMENTO
+
         // GRAVAR UM ARQUIVO DE TEXTO
     }
 }
