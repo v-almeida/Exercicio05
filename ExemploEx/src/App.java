6f98a8aa2c4f6f25cc9afc7b355cd09e50390271
Vítor <almeida.vitor20000@gmail.com>
2024-05-20 20:01:52 -0300

    thworing execption and processing some strings

--- ExemploEx/src/App.java
+++ ExemploEx/src/App.java
@@ -1,5 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws FileNotFoundException {
+        // LER O ARQUIVO DE TEXTO
+        File arquivoLeitura = new File(
+                "C:\\Users\\autologon\\Documents\\vitor-java\\lista05\\Exercicio05\\ExemploEx\\src\\arquivo.txt");
+        Scanner scanner = new Scanner(arquivoLeitura);
+        // Enquanto estiver linha no arquivo
+        while (scanner.hasNextLine()) {
+            String linha = scanner.nextLine();
+            System.out.println(linha);
+        }
+        // GRAVAR UM ARQUIVO DE TEXTO
     }
 }
